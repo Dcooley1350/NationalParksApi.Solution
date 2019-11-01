081f3d984ad3351c2f70e8a5e9561f351c0e8de5
Devin Cooley <dcooley1350@gmail.com>
2019-11-01 10:23:17 -0700

    Write Parks Controller

---
+++ NationalParksApi/Controllers/ParksController.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NationalParksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NationalParksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private NationalParksApiContext _db;
+        public ParksController(NationalParksApiContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Park park)
+        {
+            _db.Parks.Add(park);
+            _db.SaveChanges();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Park> Get(int id)
+        {
+            return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Park>> GetAction(string name, string description, string location)
+        {
+            var query = _db.Parks.AsQueryable();
+
+            if (name != null)
+            {
+                query = query.Where(entry => entry.Name == name);
+            }
+            if (description != null)
+            {
+                query = query.Where(entry => entry.Description == description);
+            }
+            if (location != null)
+            {
+                query = query.Where(entry => entry.Location == location);
+            }
+            return query.ToList();
+        
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Park park)
+        {
+            park.ParkId = id;
+            _db.Entry(park).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
